04a6c00903fdeaf0349519e56ac545aafc2522d6
vinaybongale1010 <vinaybongale1010@gmail.com>
2018-09-09 21:08:59 +0530

    creates random user

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,5 +1,11 @@
 class BoardsController < ApplicationController
   def show
+  	create_random_user
   	@board = Board.first
   end
+
+  private
+    def create_random_user
+    	User.create_random_user
+    end
 end
